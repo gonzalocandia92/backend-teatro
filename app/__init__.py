572a5cb1f3588cf853c896d3242925e821e6353a
gonzalocandia92 <gonzaloismael.cg@gmail.com>
2023-11-30 20:13:40 -0300

    contrasñea de usuario admin

--- app/__init__.py
+++ app/__init__.py
@@ -65,6 +65,22 @@ def create_default_user():
             user_datastore.add_role_to_user(admin_user, role)
     db.session.commit()
 
+def create_admin_user():
+    email = 'admin@admin.com'
+    password = 'admin123'
+
+    admin_user = user_datastore.find_user(email=email)
+    if admin_user:
+        # Si el usuario ya existe, actualiza la contraseña
+        admin_user.password = generate_password_hash(password)
+    else:
+        # Si el usuario no existe, crea uno nuevo como administrador
+        admin_user = user_datastore.create_user(email=email, password=generate_password_hash(password))
+        admin_role = user_datastore.find_role('administrador')
+        user_datastore.add_role_to_user(admin_user, admin_role)
+
+    db.session.commit()
+    print('Usuario administrador creado o actualizado exitosamente.')
 
 from app import routes
 from app.models import User, Role        
